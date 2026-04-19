d515dadfd57da52e46bb1a78e01d42eb7c202950
Андрей Землянкин <afpcp75@gmail.com>
2026-04-19 13:19:48 +0300

    a

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    #'DEFAULT_PAGINATION_CLASS': None,
+    # 'DEFAULT_PAGINATION_CLASS': None,
 }
 
 if 'pytest' in sys.argv:
